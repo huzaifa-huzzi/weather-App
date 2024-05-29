989f56359c6568f51a5704f830155bc4ccba13ab
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-05-29 16:46:50 +0500

    1.Trying to fix error

--- lib/Respository/weather_repository.dart
+++ lib/Respository/weather_repository.dart
@@ -1,12 +1,19 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_apis/Model/Weather_Api_Model.dart';
 
 class WeatherRepository {
+
+
   Future<WeatherApiModel> fetch() async {
     String url = 'https://api.openweathermap.org/data/3.0/onecall?lat=40.7108954265027&lon=-74.01161483217898&appid=005e993f8012d36439ab721c672d9d19';
 
     final response = await http.get(Uri.parse(url));
+
+    if (kDebugMode) {
+      print(response.body);
+    }
 
     if (response.statusCode == 200) {
       final body = jsonDecode(response.body);
--- lib/Routes/RoutesInitillizing.dart
+++ lib/Routes/RoutesInitillizing.dart
@@ -8,7 +8,7 @@ class Routes {
   static Route<dynamic>generateRoute(RouteSettings settings){
     switch(settings.name){
       case RoutesName.homeScreen:
-        return MaterialPageRoute(builder: (context) => HomeScreen());
+        return MaterialPageRoute(builder: (context) =>const  HomeScreen());
 
 
       default:
