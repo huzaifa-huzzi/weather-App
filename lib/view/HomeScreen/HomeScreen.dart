9524fb5d8fc66ad33afe9e22cb5bde2ae67bd482
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-05-30 18:25:27 +0500

    1.Making Design and implementation of Api

--- lib/view/HomeScreen/HomeScreen.dart
+++ lib/view/HomeScreen/HomeScreen.dart
@@ -38,56 +38,131 @@ class _HomeScreenState extends State<HomeScreen> {
             } else {
               String cityName = snapshot.data!.timezone?.split('/')?.last ?? '';
               var weatherIcon = snapshot.data!.current!.weather?.first.icon ?? '';
+              var weatherDescription = snapshot.data!.current!.weather?.first.description ?? '';
 
-              return Column(
+              return ListView(
                 children: [
-                  SizedBox(height: height * .03,),
-                  Padding(
-                    padding:const  EdgeInsets.only(right: 170),
-                    child: Column(
-                      children: [
-                        Text(
-                            cityName,
-                            style: const TextStyle(fontSize: 35),
-                          ),
-                        Text(
-                          currentDate,
-                          style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  Column(
+                    children: [
+                      SizedBox(height: height * .03,),
+                      Padding(
+                        padding:const  EdgeInsets.only(right: 170),
+                        child: Column(
+                          children: [
+                            Text(
+                              cityName,
+                              style: const TextStyle(fontSize: 35),
+                            ),
+                            Text(
+                              currentDate,
+                              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: height * .06,),
-                  Padding(
-                    padding:const  EdgeInsets.only(left: 30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Image.asset(
-                          'assets/weather/$weatherIcon.png',
-                          height: 50,
-                          width: 50,
-                        ),
-                        Padding(
-                          padding:const  EdgeInsets.only(right: 20),
-                          child: RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: '${snapshot.data!.current!.temp.toString()}\u00B0',
-                                  style:const  TextStyle(fontSize: 27, color: Colors.black), // Define style here
+                      ),
+                      SizedBox(height: height * .06,),
+                      Padding(
+                        padding:const  EdgeInsets.only(left: 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              'assets/weather/$weatherIcon.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                            Padding(
+                              padding:const  EdgeInsets.only(right: 20),
+                              child: RichText(
+                                  text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: '${snapshot.data!.current!.temp.toString()}\u00B0',
+                                          style:const  TextStyle(fontSize: 40, color: Colors.black,fontWeight: FontWeight.w600), // Define style here
+                                        ),
+                                        TextSpan(
+                                          text: weatherDescription,
+                                          style:const  TextStyle(fontSize: 14, color: Colors.grey,fontWeight: FontWeight.w400), // Define style here
+                                        ),
+
+                                      ]
+                                  )
+                              ),
+                            )
+                          ],),
+                      ),
+                      SizedBox(height: height * .06,),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Container(
+                                height:60,
+                                width: 60,
+                                padding:const  EdgeInsets.all(16),
+                                decoration: BoxDecoration(
+                                    color: Colors.grey.shade300,
+                                    borderRadius: BorderRadius.circular(15)
                                 ),
+                                child: Image.asset('assets/icons/windspeed.png'),
+                              ),
+                              Container(
+                                height:60,
+                                width: 60,
+                                padding:const  EdgeInsets.all(16),
+                                decoration: BoxDecoration(
+                                    color: Colors.grey.shade300,
+                                    borderRadius: BorderRadius.circular(15)
+                                ),
+                                child: Image.asset('assets/icons/clouds.png'),
 
-                          ]
-                        )
+                              ),
+                              Container(
+                                height:60,
+                                width: 60,
+                                padding:const  EdgeInsets.all(16),
+                                decoration: BoxDecoration(
+                                    color: Colors.grey.shade300,
+                                    borderRadius: BorderRadius.circular(15)
+                                ),
+                                child: Image.asset('assets/icons/humidity.png'),
+
+                              )
+                            ],
                           ),
-                        )
-                      ],),
-                  )
+                          SizedBox(height: height * .01,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              SizedBox(
+                                height: 20,
+                                width: 60,
+                                child: Text("${snapshot.data!.current!.windSpeed.toString()} km/h",style: const TextStyle(fontSize: 12),textAlign: TextAlign.center,),
+                              ),
+                              SizedBox(
+                                height: 20,
+                                width: 60,
+                                child: Text("${snapshot.data!.current!.clouds.toString()} %",style: const TextStyle(fontSize: 12),textAlign: TextAlign.center,),
+                              ),
+                              SizedBox(
+                                height: 20,
+                                width: 60,
+                                child: Text("${snapshot.data!.current!.humidity.toString()} %",style: const TextStyle(fontSize: 12),textAlign: TextAlign.center,),
+                              ),
+
+                            ],
+                          )
+                        ],
+                      )
 
 
+                    ],
+                  ),
                 ],
               );
+
+
             }
           },
         ),
