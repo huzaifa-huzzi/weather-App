002058a95d0a642d749ab3c36d0a8b51d1041da8
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-06-04 14:14:54 +0500

    1.Last Commit

--- lib/view/Main Screen/MainScree.dart
+++ lib/view/Main Screen/MainScree.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:weather_apis/Model/Weather_Api_Model.dart';
+import 'package:weather_apis/Utils/Colors.dart';
 import 'dart:async' as async;
 
 import 'package:weather_apis/view/HomeScreen/HomeScreen.dart';
+import 'package:weather_apis/view_model/Weather_view_model.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -12,6 +15,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  WeatherViewModel weatherViewModel = WeatherViewModel();
 
   @override
   void initState() {
@@ -28,17 +32,34 @@ class _MainScreenState extends State<MainScreen> {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-            child: Column(
+      body:FutureBuilder<WeatherApiModel>(
+        future: weatherViewModel.fetch(),
+        builder: (BuildContext context, AsyncSnapshot<WeatherApiModel> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: SpinKitFadingCircle(color: Colors.blue),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset('assets/Icons/clouds.png'),
-                SizedBox(height: height * .05,),
-                const SpinKitFadingCircle(color: Colors.blue)
+                Image.asset('assets/icons/clouds.png'),
+               SizedBox(height: height * .04,),
+               const  SpinKitFadingCircle(color: CustomColors.firstGradientColor,)
               ],
-            ),
-          )
+            );
+          }
+        },
       ),
+
+
+
+
     );
   }
 }
